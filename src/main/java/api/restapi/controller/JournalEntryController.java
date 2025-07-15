6db8b55235eb6b71560398fb0add14104f6869df
raviraj0007 <raviraj04066@gmail.com>
2025-07-15 11:18:31 +0530

    CORS ERROR FIXED

--- src/main/java/api/restapi/controller/JournalEntryController.java
+++ src/main/java/api/restapi/controller/JournalEntryController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/journal")
 public class JournalEntryController {
